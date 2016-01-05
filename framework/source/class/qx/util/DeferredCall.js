6cd12bb4d89cf5f63258f03fb8e5c27914abae53
Cajus Pollmeier <pollmeier@gonicus.de>
2016-01-05 14:33:42 +0100

    Revert "Skip call if disposed"
    This reverts commit 32392c206e6fc66bc0d291847bd0c96ebefabfd0.

--- framework/source/class/qx/util/DeferredCall.js
+++ framework/source/class/qx/util/DeferredCall.js
@@ -95,7 +95,6 @@ qx.Class.define("qx.util.DeferredCall",
             "The context object '" + context + "' of the defered call '" +
             this + "'is already disposed."
           );
-          return;
         }
       }
 
